20578b3d1da3bab22e4eada099543be6d9f44031
pedromartinscs <pedro@nancode.com.br>
2019-10-25 16:34:01 -0300

    Vote model

--- UrnaLivre/UrnaLivre/Model/Block.cs
+++ UrnaLivre/UrnaLivre/Model/Block.cs
@@ -9,6 +9,15 @@ namespace UrnaLivre
     /// </summary>
     public class Block
     {
+        public string BlockNumber { get; set; }
+        public string Hash { get; set; }
+        public string ParentHash { get; set; }
+        public string Nonce { get; set; }
+        public string ExtraData { get; set; }
+        public string Size { get; set; }
+        public string Miner { get; set; }
+        public string Votes { get; set; }
+        public string Timestamp { get; set; }
 
     }
 }
---
+++ UrnaLivre/UrnaLivre/Model/Vote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrnaLivre
+{
+    public class Vote
+    {
+        public string BlockHash { get; set; }
+        public string BlockNumber { get; set; }
+        public string Hash { get; set; }
+        public string TimeStamp { get; set; }
+        public string Candidate { get; set; }
+        public string SecKey { get; set; }
+        public string PublicKey { get; set; }
+    }
+}
