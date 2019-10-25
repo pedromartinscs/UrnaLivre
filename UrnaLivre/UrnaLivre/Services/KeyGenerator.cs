9f83dae2570762d67b4f17554e7c79221af7f3f0
pedromartinscs <pedro@nancode.com.br>
2019-10-25 18:42:25 -0300

    KeyGenerator

---
+++ UrnaLivre/UrnaLivre/Services/KeyGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace UrnaLivre.Services
+{
+    public class KeyGenerator : ECDsa
+    {
+        public override byte[] SignHash(byte[] hash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool VerifyHash(byte[] hash, byte[] signature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
